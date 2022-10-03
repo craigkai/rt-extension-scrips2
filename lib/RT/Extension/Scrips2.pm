c778c4b9ffd94c2781943f068a54181fa35103ea
craig kaiser <craig@kaiserengineering.io>
2022-10-03 10:37:02 -0400

    init

---
+++ lib/RT/Extension/Scrips2.pm
@@ -0,0 +1,73 @@
+use strict;
+use warnings;
+package RT::Extension::Scrips2;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+RT-Extension-Scrips2 - [One line description of module's purpose here]
+
+=head1 DESCRIPTION
+
+[Why would someone install this extension? What does it do? What problem
+does it solve?]
+
+=head1 RT VERSION
+
+Works with RT [What versions of RT is this known to work with?]
+
+[Make sure to use requires_rt and rt_too_new in Makefile.PL]
+
+=head1 INSTALLATION
+
+=over
+
+=item C<perl Makefile.PL>
+
+=item C<make>
+
+=item C<make install>
+
+May need root permissions
+
+=item Edit your F</opt/rt4/etc/RT_SiteConfig.pm>
+
+Add this line:
+
+    Plugin('RT::Extension::Scrips2');
+
+=item Clear your mason cache
+
+    rm -rf /opt/rt4/var/mason_data/obj
+
+=item Restart your webserver
+
+=back
+
+=head1 AUTHOR
+
+Best Practical Solutions, LLC E<lt>modules@bestpractical.comE<gt>
+
+=for html <p>All bugs should be reported via email to <a
+href="mailto:bug-RT-Extension-Scrips2@rt.cpan.org">bug-RT-Extension-Scrips2@rt.cpan.org</a>
+or via the web at <a
+href="http://rt.cpan.org/Public/Dist/Display.html?Name=RT-Extension-Scrips2">rt.cpan.org</a>.</p>
+
+=for text
+    All bugs should be reported via email to
+        bug-RT-Extension-Scrips2@rt.cpan.org
+    or via the web at
+        http://rt.cpan.org/Public/Dist/Display.html?Name=RT-Extension-Scrips2
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2022 by Craig Kaiser
+
+This is free software, licensed under:
+
+  The GNU General Public License, Version 2, June 1991
+
+=cut
+
+1;
